39f436c2f16cd2a810b35cc548a181632bc84f38
SylvainJanet <74711059+SylvainJanet@users.noreply.github.com>
2020-11-20 10:45:55 +0100

    Generic Repository Added Base entity model (Id) Changed viewmodel name in views (bug)

---
+++ Shop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,88 @@
+﻿using Shop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepository()
+        {
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T item)
+        {
+            items.Add(item);
+        }
+
+        public void Update(T item)
+        {
+            T dbitem = items.Find(i => item.Id == i.Id);
+            if (dbitem != null)
+            {
+                foreach (var prop in typeof(T).GetProperties())
+                {
+                    if (prop.Name != "Id")
+                    {
+                        var property = typeof(T).GetProperty(prop.Name);
+                        property.SetValue(dbitem, Convert.ChangeType(property.GetValue(item), prop.PropertyType));
+                    } 
+                }
+            }
+            else
+            {
+                throw new Exception("Item of class "+className+" not found");
+            }
+        }
+
+        public T FindById(int id)
+        {
+            T item = items.SingleOrDefault(i => id == i.Id);
+            if (item != null)
+            {
+                return item;
+            }
+            else
+            {
+                throw new Exception("Item of class " + className + " not found");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(int id)
+        {
+            T item = items.SingleOrDefault(i => id == i.Id);
+            if (item != null)
+            {
+                items.Remove(item);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+    }
+}
